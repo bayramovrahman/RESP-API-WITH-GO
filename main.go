f955538c3e55620bd664785bb3b3ab52f5ce63c9
Rahman Bayramov <bayramovrahman@gmail.com>
2025-04-04 21:51:40 +0500

    GET & POST processing successfully enabled

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import(
-	"fmt"
+import (
+	"net/http"
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Rest API - Project")
+	server := gin.Default()
+
+	// Basit bir GET endpoint'i oluştur
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	// 8080 portunda çalıştır
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		// context.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	
+	context.JSON(http.StatusCreated, gin.H{"message": "The event successfully created!", "event": event})
 }
