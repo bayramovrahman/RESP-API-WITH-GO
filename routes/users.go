0add12e940f637d731a55bcffd650670808cbd36
Rahman Bayramov <bayramovrahman@gmail.com>
2025-04-13 21:44:57 +0500

    Login processing successfully enabled!

--- routes/users.go
+++ routes/users.go
@@ -22,3 +22,21 @@ func signUp(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "The user successfully created!", "user": user})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data: " + err.Error()})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenticate user: " + err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successfully!"})
+}
--- utils/hash.go
+++ utils/hash.go
@@ -6,3 +6,15 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+
+func CheckPasswordHash(password, hashedPassword string) bool {
+	err :=  bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	/*
+		Hata umurumda değil, bunun yerine bu işlemi burada döndürerek, 
+		şifre geçersizse false döndürüyorum çünkü şifre geçerliyse 
+		hata nil olacak ve şifre geçersizse nil olmayacak.
+	*/
+
+	return err == nil
+}
